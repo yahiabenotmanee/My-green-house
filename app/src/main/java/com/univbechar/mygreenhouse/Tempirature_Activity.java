fdeda05ef92a7f08aee59497a41a0c089cea733a
yahia benotmane <benotmane.yahia@univ-bechar.dz>
2025-05-31 10:06:26 +0100

    v6 add graph to Tempirature_Activity

--- app/src/main/java/com/univbechar/mygreenhouse/Tempirature_Activity.java
+++ app/src/main/java/com/univbechar/mygreenhouse/Tempirature_Activity.java
@@ -2,7 +2,11 @@ package com.univbechar.mygreenhouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Tempirature_Activity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class Tempirature_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tempirature);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageView Image_close = findViewById(R.id.image_close);
+        Image_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Tempirature_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
     }
 }
